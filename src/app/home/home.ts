eb5040df456d19fe60d00c4f3a8b82b1daabc879
Dilna K Vasavan <dilna@xminds.com>
2025-09-26 15:57:58 +0530

    Fragments in Routes

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit{
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
 
+    ngOnInit(){
+        this.activeRoute.fragment.subscribe((data) => {
+            this.jumpToSection(data);
+        });
+    }
+
+    jumpToSection(section){
+      document.getElementById(section).scrollIntoView({behavior:'smooth'})
+    }
 }
